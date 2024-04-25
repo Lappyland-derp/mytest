8490a5268e7ab72e379fbb13127aad7790e52b6f
Lappyland-derp <lappyland1102@gmail.com>
2024-04-26 05:31:28 +0800

    ass

--- backend/base/models.py
+++ backend/base/models.py
@@ -1,4 +1,17 @@
 from django.db import models
 
 
+#class Product(models.Model):
+#    user = models.ForeignKey(User, on_delete=models.SET_NUll, null=True)
+#    name = models.CharField(max_length=200, null=True, blank=True)
+#    category = models.CharField(max_length=200, null=True, blank=True)
+#    description = models.TextField(null=True, blank=True)
+#   rating = models.DecimalField(max_digits=7, decimals_places=2, null=True, blank=True)
+#    numReviews = models.IntegerField(null=True, blank=True, default=0)
+#    price = models.DecimalField(max_digits=7, decimals_places=2, null=True, blank=True)
+#   createdAt = models.DateTimeField(auto_now_add=True)
+#    _id = models.AutoField(primary_key=True)
+
+#    def_str_(self):
+#            return self.name
 # Create your models here.
